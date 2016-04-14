aeed8671a4c7ff3c2eab731e85f92c9a450d604c
Sam Kristoff <samkristoff@github.com>
2016-04-14 16:51:56 -0500

    Add oscope 'single' mode with basic graphing

--- app/components/charts/oscilloscope/oscilloscope-chart.component.ts
+++ app/components/charts/oscilloscope/oscilloscope-chart.component.ts
@@ -7,31 +7,87 @@ import {CHART_DIRECTIVES, Highcharts} from 'angular2-highcharts';
     selector: 'oscilloscope-chart',
     templateUrl: 'build/components/charts/oscilloscope/oscilloscope-chart.html',
     directives: [CHART_DIRECTIVES, IONIC_DIRECTIVES],
+    viewProviders: [HTTP_PROVIDERS],
 })
 export class OscilloscopeChartComponent {
-    private refreshData = "empty";
+    private chart: Object;
     private options: Object;
-    private data: any = 'test';
-    
-    constructor(http: Http) {
+    private data: Array<number>;
+
+    public sampleRate: number;
+    public numSamples: number;
+    public sigFreq: number;
+    public phaseOffset: number;
+
+    constructor(private http: Http) {
+        this.sampleRate = 10000;
+        this.numSamples = 100;
+        this.sigFreq = 200;
+        this.phaseOffset = 0;
+
         this.options = {
             chart: {
                 type: 'line',
                 zoomType: 'x',
+                title: ''
             },
-            series: [{
-                data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-            }]
-        };
-        
-        //http.get('people.json').map(res => res.json()).subscribe(data => this.data = data);
-    }
-    
+            tooltip: {
+                shared: true,
+                formatter: function() {
+                    let tip = '<b>' + this.x.toFixed(2) + '</b>';
+                    this.points.forEach(function(point){
+                        tip += '<br/>' + point.series.name + ': ' + point.y.toFixed(2) + ' MEGAVOLTS!';
+                    });
+                    
+                    return tip;
+                },
+            },
+            series: [
+                {
+                    name: 'Channel 0',
+                    data: [],
+                },
+                {
+                    name: 'Channel 1',
+                    data: [],
+                }
+            ]
 
-    refresh() {
-        //this.refreshData = this.http.post("https://0u7h6sgzf6.execute-api.us-east-1.amazonaws.com/prod/", "{'operation': 'sin'}").map(function(response) { return response.json(); });
-        alert(
-            this.data
-        );
+        };
+    }
+
+    saveInstance(instance) {
+        this.chart = instance;
+    }
+
+    refresh(sampleRate, numSamples, sigFreq, phaseOffset) {
+        let url = 'https://0u7h6sgzf6.execute-api.us-east-1.amazonaws.com/prod';
+        let params = {
+            "sigType": "sin",
+            "sampleRate": sampleRate,
+            "numSamples": numSamples,
+            "sigFreq": sigFreq,
+            "phaseOffset": phaseOffset
+        };
+
+        this.http.post(url, JSON.stringify(params)).subscribe((response) => {
+            if (response.status != 200) {
+                console.log(Error, 'Bad Response: ', response);
+            }
+            else {
+                let waveform = JSON.parse(response.text());
+
+                this.drawWaveform(this.chart, 0, waveform);
+
+
+            }
+        });
+    }
+
+    drawWaveform(chart, seriesNum, waveform) {
+        chart.options.tooltip.valueSuffix = 'Dags';
+        chart.series[seriesNum].options.pointStart = waveform.t0;
+        chart.series[seriesNum].options.pointInterval = waveform.dt;
+        chart.series[seriesNum].setData(waveform.y);
     }
 }
