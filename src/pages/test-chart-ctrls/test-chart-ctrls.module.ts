daaa5bf5a8855a30c48eb990730c1740c7b91e4d
Sudharsan Sukumar <sudharsan.sukumar5@gmail.com>
2016-10-03 05:20:33 +0100

    Add extra popover and cursor modals to instrument panel module

--- src/pages/test-chart-ctrls/test-chart-ctrls.module.ts
+++ src/pages/test-chart-ctrls/test-chart-ctrls.module.ts
@@ -14,14 +14,29 @@ import { TriggerComponent } from '../../components/trigger/trigger.component';
 import { FgenComponent } from '../../components/function-gen/function-gen.component';
 import { DigitalIoComponent } from '../../components/digital-io/digital-io.component';
 import { DcSupplyComponent } from '../../components/dc-supply/dc-supply.component';
+import { ModalCursorPage } from '../../pages/cursor-modal/cursor-modal';
+import { MathModalPage } from '../../pages/math-modal/math-modal';
+import { SeriesPopover } from '../../components/series-popover/series-popover.component';
+import { TriggerPopover } from '../../components/trigger-popover/trigger-popover.component';
+import { ModalFgenPage } from '../../pages/fgen-modal/fgen-modal';
 
 @NgModule({
     imports: [
         SharedModule,
-        IonicModule.forRoot(TestChartCtrlsPage)
+        IonicModule.forRoot(TestChartCtrlsPage),
+        IonicModule.forRoot(ModalCursorPage),
+        IonicModule.forRoot(MathModalPage),
+        IonicModule.forRoot(ModalFgenPage),
+        IonicModule.forRoot(SeriesPopover),
+        IonicModule.forRoot(TriggerPopover)
     ],
     declarations: [
         TestChartCtrlsPage,
+        ModalFgenPage,
+        ModalCursorPage,
+        MathModalPage,
+        TriggerPopover,
+        SeriesPopover,
         SilverNeedleChart,
         BottomBarComponent,
         XAxisComponent,
