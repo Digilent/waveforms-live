2bc4442c527e5eea0dbc40013f5165ce62c2fab7
Sudharsan Sukumar <sudharsan.sukumar5@gmail.com>
2016-10-19 23:56:37 +0100

    Implement changing chart colors (WIP)

--- src/pages/chart-modal/chart-modal.ts
+++ src/pages/chart-modal/chart-modal.ts
@@ -14,6 +14,7 @@ export class ChartModalPage {
     public viewCtrl: ViewController;
     public params: NavParams;
     public chart: SilverNeedleChart;
+    public colorPickerRef: HTMLElement;
 
     constructor(
         _platform: Platform,
@@ -26,10 +27,33 @@ export class ChartModalPage {
         this.chart = this.params.get('chartComponent');
     }
 
+    ngOnInit() {
+        this.colorPickerRef = document.getElementById('color-picker');
+    }
+
     
     //Export series as csv with file name 'myData'
     exportChart() {
         this.chart.exportCsv('myData');
+    }
+
+    openColorPicker(event) {
+        this.colorPickerRef.addEventListener('input', (event: any) => {
+            this.chart.chart.series[0].update({
+                color: event.target.value
+            }, true);
+            if (this.chart.timelineView) {
+                this.chart.timelineChart.series[0].update({
+                    color: event.target.value
+                }, true);
+            }
+            if (this.chart.seriesAnchors[0] !== undefined) {
+                this.chart.seriesAnchors[0].attr({
+                    fill: event.target.value
+                });
+            }
+        });
+        this.colorPickerRef.click();
     }
 
     close() {
