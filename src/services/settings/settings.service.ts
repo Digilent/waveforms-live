95c351bd85141350651ee3d928a84de2aa64ff92
Andrew Holzer <andrew.holzer@digilent.com>
2018-02-02 18:45:29 +0000

    Update version to 1.3.4

--- src/services/settings/settings.service.ts
+++ src/services/settings/settings.service.ts
@@ -17,7 +17,7 @@ export class SettingsService {
     public nestedChannels: boolean = false;
     public routeToStore: boolean = true;
     public drawLaOnTimeline: boolean = false;
-    public wflVersion: string = '1.3.3';
+    public wflVersion: string = '1.3.4';
     public useDevBuilds: boolean = false;
     public androidAppLink = "market://details?id=com.digilent.waveformslive";
     public iosAppLink = "https://itunes.apple.com/us/app/waveforms-live/id1244242035";
